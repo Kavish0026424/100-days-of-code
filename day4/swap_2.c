eb47f412b0d2915153b7f1b2569cc8c2034af65d
Kavish0026424 <Kavish.26424@stu.upes.ac.in>
2025-09-01 19:32:35 +0530

    Create swap_2.c

---
+++ day4/swap_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter first no :");
+    scanf("%d",&a);
+
+    printf("Enter second no. :");
+    scanf("%d",&b);
+
+    printf("Before swap :%d %d\n",a ,b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After swap :%d %d",a ,b);
+    return 0;
+}
